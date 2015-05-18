e7db992b0a927d0b08b229b8ab66c5cd7dbc1583
unknown <m.hesse@QAMUC-NB-0490.muc.qaware.local>
2015-05-18 12:55:33 +0200

    Fixing hook placement

--- app/src/main/java/de/tum/inappbillinghack/GetBuyIntentHook.java
+++ app/src/main/java/de/tum/inappbillinghack/GetBuyIntentHook.java
@@ -4,13 +4,14 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 
 public class GetBuyIntentHook extends XC_MethodHook {
     @Override
-    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+    /*protected void afterHookedMethod(MethodHookParam param) throws Throwable {
         XposedBridge.log("hook");
 
         Bundle bundle = new Bundle();
@@ -30,5 +31,35 @@ public class GetBuyIntentHook extends XC_MethodHook {
 
         // replace original intent with our intent
         param.setResult(bundle);
+    }*/
+
+    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+        XposedBridge.log("hook");
+
+        // getBuyIntent
+        if ((Integer) param.args[0] == 3) {
+            XposedBridge.log("getBuyIntent");
+            Bundle bundle = new Bundle();
+            bundle.putInt(IabHelper.RESPONSE_CODE, IabHelper.BILLING_RESPONSE_RESULT_OK);
+
+            Context c = new BuyIntent();
+            Parcel parcel = (Parcel) param.args[1];
+            parcel.enforceInterface("com.android.vending.billing.IInAppBillingService");
+            parcel.readInt();
+            
+
+            // create our own intent
+            Intent intent = new Intent();
+            intent.setClass(c, BuyIntent.class);
+            intent.setAction(BuyIntent.BUY_INTENT);
+            intent.putExtra(BuyIntent.EXTRA_PACKAGENAME, parcel.readString());
+            intent.putExtra(BuyIntent.EXTRA_PRODUCT_ID, parcel.readString());
+            intent.putExtra(BuyIntent.EXTRA_DEV_PAYLOAD, parcel.readString());
+
+            bundle.putParcelable(IabHelper.RESPONSE_BUY_INTENT, PendingIntent.getActivity(c, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+            // replace original intent with our intent
+            param.setResult(bundle);
+        }
     }
 }
--- app/src/main/java/de/tum/inappbillinghack/MainClass.java
+++ app/src/main/java/de/tum/inappbillinghack/MainClass.java
@@ -1,30 +1,64 @@
 package de.tum.inappbillinghack;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Parcel;
 import android.widget.TextView;
+
+import com.android.vending.billing.IInAppBillingService;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
+import org.apache.commons.lang3.ClassUtils;
+
+import java.lang.reflect.Method;
+
 import static de.robv.android.xposed.XposedHelpers.*;
 
 
 public class MainClass implements IXposedHookLoadPackage {
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.android.systemui"))
-            return;
-        XposedBridge.log("Loaded app: " + lpparam.packageName);
+        if (lpparam.packageName.equals("com.android.systemui")) {
+            findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    TextView tv = (TextView) param.thisObject;
+                    //tv.setText(new StringBuilder(tv.getText().toString()).reverse().toString());
+                    tv.setRotation(180);
+                }
+            });
+        } else {
+            try {
+                ClassUtils.getClass(lpparam.classLoader, "com.android.vending.billing.IInAppBillingService.Stub");
 
-        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                TextView tv = (TextView) param.thisObject;
-                tv.setText(new StringBuilder(tv.getText().toString()).reverse().toString());
-            }
-        });
+                XposedBridge.log("hooking in package " + lpparam.packageName);
+                findAndHookMethod("com.android.vending.billing.IInAppBillingService.Stub", lpparam.classLoader,
+                        "onTransact", int.class, Parcel.class, Parcel.class, int.class, new GetBuyIntentHook());
+
+            } catch(ClassNotFoundException e) {}
+        }
+        /*XposedBridge.log("Loaded app: " + lpparam.packageName);
+
+
 
         findAndHookMethod("com.android.vending.billing.IInAppBillingService.Stub", lpparam.classLoader,
-                "getBuyIntent", int.class, String.class, String.class, String.class, String.class, new GetBuyIntentHook());
+                "onTransact", int.class, Parcel.class, Parcel.class, int.class, new GetBuyIntentHook());
+
+        /*Class<?> stub = IInAppBillingService.class.getDeclaredClasses()[0].getDeclaredClasses()[0];
+        Method[] m = stub.getDeclaredMethods();
+        XposedBridge.log("methods:");
+        for (Method me:m) {
+            XposedBridge.log("method: " + me.getName());
+            if (me.getName().equalsIgnoreCase("getBuyIntent")) {
+                XposedBridge.log("hook here..");
+                XposedBridge.hookMethod(me, new GetBuyIntentHook());
+            }
+        }*/
     }
 }
