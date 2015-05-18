ae9feab2c2d25650b346dfa7b8654b41e88a0c54
unknown <m.hesse@QAMUC-NB-0490.muc.qaware.local>
2015-05-18 15:53:58 +0200

    Still not working

--- app/src/main/java/de/tum/inappbillinghack/MainClass.java
+++ app/src/main/java/de/tum/inappbillinghack/MainClass.java
@@ -1,14 +1,8 @@
 package de.tum.inappbillinghack;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
+import android.os.Bundle;
 import android.os.Parcel;
 import android.widget.TextView;
-
-import com.android.vending.billing.IInAppBillingService;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -16,8 +10,6 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import org.apache.commons.lang3.ClassUtils;
-
-import java.lang.reflect.Method;
 
 import static de.robv.android.xposed.XposedHelpers.*;
 
@@ -30,35 +22,18 @@ public class MainClass implements IXposedHookLoadPackage {
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     TextView tv = (TextView) param.thisObject;
                     //tv.setText(new StringBuilder(tv.getText().toString()).reverse().toString());
-                    tv.setRotation(180);
+                    tv.setRotation(((System.currentTimeMillis()/100000)%8)*45);
                 }
             });
         } else {
             try {
                 ClassUtils.getClass(lpparam.classLoader, "com.android.vending.billing.IInAppBillingService.Stub");
-
                 XposedBridge.log("hooking in package " + lpparam.packageName);
+
                 findAndHookMethod("com.android.vending.billing.IInAppBillingService.Stub", lpparam.classLoader,
                         "onTransact", int.class, Parcel.class, Parcel.class, int.class, new GetBuyIntentHook());
 
             } catch(ClassNotFoundException e) {}
         }
-        /*XposedBridge.log("Loaded app: " + lpparam.packageName);
-
-
-
-        findAndHookMethod("com.android.vending.billing.IInAppBillingService.Stub", lpparam.classLoader,
-                "onTransact", int.class, Parcel.class, Parcel.class, int.class, new GetBuyIntentHook());
-
-        /*Class<?> stub = IInAppBillingService.class.getDeclaredClasses()[0].getDeclaredClasses()[0];
-        Method[] m = stub.getDeclaredMethods();
-        XposedBridge.log("methods:");
-        for (Method me:m) {
-            XposedBridge.log("method: " + me.getName());
-            if (me.getName().equalsIgnoreCase("getBuyIntent")) {
-                XposedBridge.log("hook here..");
-                XposedBridge.hookMethod(me, new GetBuyIntentHook());
-            }
-        }*/
     }
 }
